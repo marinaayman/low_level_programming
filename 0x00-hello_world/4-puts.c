b7149d5efcc1c9f9cf34c265bed2c6a0ebe14141
marinaayman <marinaaymankaram@gmail.com>
2023-03-16 06:53:47 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program uses puts function to print text'
+ * @parameter: 
+ *            str1 : contain the text to be printed.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
