be264943bb8234bd0f5bcfd1c4851dc4253a90c4
marinaayman <marinaaymankaram@gmail.com>
2023-03-16 09:25:13 -0700

    0. Preprocessor

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: 'the program uses puts function to print text'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		
+}
