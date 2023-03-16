a56b5d65c8a91189b51d404106538c4513e1aaaf
marinaayman <marinaaymankaram@gmail.com>
2023-03-16 11:28:06 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Entry
+  * main - Entry Point
   * Description : 'the program display a quote with putchar function'
   */
 int main(void)
@@ -9,9 +9,9 @@ int main(void)
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int counter = 0;
 
-	while(str[counter] != '\0')
+	while (str[counter] != '\0')
 	{
 		putchar(str[counter++]);
-	}	
+	}
 	return (1);
 }
