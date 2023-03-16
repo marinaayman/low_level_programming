06cf6fd976c3758a7adad8d54f8a120ed725c1d4
marinaayman <marinaaymankaram@gmail.com>
2023-03-16 10:18:33 -0700

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Description: 'the program prints the sizes of computer types'
- *
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %u byte(s)\n",(unsigned int)(sizeof(char)));
-	printf("Size of an int: %u byte(s)\n",(unsigned int)(sizeof(int)));
-	printf("Size of a long int: %u byte(s)\n",(unsigned int)(sizeof(long int)));
-	printf("Size of a long long int: %u byte(s)\n",(unsigned int)(sizeof(long long int)));
-	printf("Size of a float: %u byte(s)\n",(unsigned int)(sizeof(float)));
+	char c;
+	int i;
+	long int ln;
+	long long int lln;
+	float f;
+
+	printf("Size of a char: %u byte(s)\n", (unsigned int)sizeof(c));
+	printf("Size of an int: %u byte(s)\n", (unsigned int)sizeof(i));
+	printf("Size of a long int: %u byte(s)\n", (unsigned int)sizeof(ln));
+	printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(lln));
+	printf("Size of a float: %u byte(s)\n", (unsigned int)sizeof(f));
 }
