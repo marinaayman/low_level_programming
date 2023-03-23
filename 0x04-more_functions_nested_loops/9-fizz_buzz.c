d4e15d26a92db9b12a20c4c623177cf26e91b998
marinaayman <marinaaymankaram@gmail.com>
2023-03-23 12:49:57 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,6 +12,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i == 1)
+		{
+			printf("1");
+			i++;
+		}
 		if (i % 3 == 0 && i % 5 == 0)
 			printf(" FizzBuzz ");
 		else if (i % 3 == 0)
