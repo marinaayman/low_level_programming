5ebbd3b80995a07cdac5ef4fe3e6774385c378df
marinaayman <marinaaymankaram@gmail.com>
2023-03-27 17:25:04 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int index = 0, len = 0;
 
-	while (src[index])
+	while (src[index++])
+		len++;
+
+	for (index = 0; index <= len + 1; index++)
 	{
 		dest[index] = src[index];
-		index++;
 	}
-	dest[index + 1] = '\0';
-
 	return (dest);
 }
