b6a61a7bf7f4be2e171cd018968d817b4b67f951
marinaayman <marinaaymankaram@gmail.com>
2023-04-03 18:19:01 -0700

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,27 @@
+#include "main.h"
+
+/**
+  * _strspn- gets the length of a prefix substring.
+  * @s: the string to be scanned.
+  * @accept: the string bytes to be compared with.
+  * Return: unsigned int.
+  */
+
 unsigned int _strspn(char *s, char *accept)
 {
+	char *p1, *p2;
+	*p1 = s;
+	*p2 = accept;
 
+	while (*p1)
+	{
+		p1++;
+		p2++;
+		while (*p2)
+		{
+			if (*p1 == *p2)
+				continue;
+		}
+	}
+	return (p1 - 1 - s);
 }
