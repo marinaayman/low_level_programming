8235c1b48416706a1f11c2fb7412ef23e7fe5999
marinaayman <marinaaymankaram@gmail.com>
2023-04-04 16:02:03 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	while (*dest)
+		dest++;
 
-	while (dest[index++])
-		dest_len++;
+	for (; *src != '\0'; src++)
+	{
+		*dest = *src;
+		dest++;
+	}
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	*dest = '\0';
 
 	return (dest);
-
 }
