28a9faf26582054645d93dd7eadd3a2b9238dfa6
marinaayman <marinaaymankaram@gmail.com>
2023-04-04 18:13:27 -0700

    complete

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,11 +9,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	char *a = s;
+	unsigned int bytes = 0;
+	int index;
 
 	while (*s)
-		for (; *s == *accept; s++)
-			;
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
 
-	return (s - a);
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
 }
