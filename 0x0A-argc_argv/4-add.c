f944ecdcbfd9e35b670e0f963b127db9b04f04e5
marinaayman <marinaaymankaram@gmail.com>
2023-04-10 11:32:39 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define true 1
-#define false 0
-typedef bool int;
-
-/**
-  * Isnum- checks the string contains only digits.
-  * @str: the string argument.
-  * Return : bool type 
-  *          1 if it's a number.
-  *          0 if not.
-  */
-
-bool Isnum(const char *str);
 
 /**
   * main- program that adds positive numbers.
@@ -25,20 +12,20 @@ bool Isnum(const char *str);
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (Isnum(argv[i]))
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				sum += atoi(argv[i];
 			}
 		}
 
@@ -50,17 +37,4 @@ int main(int argc, char *argv[])
 	}
 
 	return (0);
-}
-
-bool Isnum(const char *str)
-{
-	int j;
-
-	for (j = 0; str[j] != '\0'; j++)
-	{
-		if (!isdigit(str[j]))
-				return (false);
-	}
-
-	return (true);
 }
