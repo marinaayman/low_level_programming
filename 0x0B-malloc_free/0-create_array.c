9f148421641f758d4e1b69af2dee8cd93e3620a4
marinaayman <marinaaymankaram@gmail.com>
2023-04-11 09:37:42 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *ptr = malloc(size);
+	char *ptr;
 	unsigned int i;
+
+	ptr = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+	
+	if (ptr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
