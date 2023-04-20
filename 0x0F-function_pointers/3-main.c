833f4b573d01c4cb4a9db22515efc6d6e11e8894
marinaayman <marinaaymankaram@gmail.com>
2023-04-20 06:56:57 -0700

    task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,31 +12,27 @@
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2;
 	char *operator = argv[2];
 	int (*func_ptr)(int, int) = get_op_func(operator);
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (((strspn(argv[1], "0123456789") != strlen(argv[1])) ||
-	   (strspn(argv[3], "0123456789") != strlen(argv[3]))) ||
+	if ((strspn(argv[1], "0123456789") != strlen(argv[1])) ||
+	   (strspn(argv[3], "0123456789") != strlen(argv[3])) ||
 	   (get_op_func(operator) == NULL || *(operator + 1) != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*operator == '/' || *operator == '%') && num2 == 0)
+	if ((*operator == '/' || *operator == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", func_ptr(num1, num2));
+	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 
 }
