42d6e74cdac21805bea45c7f0c55cc6f4289ca5a
marinaayman <marinaaymankaram@gmail.com>
2023-04-20 14:51:33 -0700

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(args, n);
-	if (separator == NULL || n == 0)
+	if (separator == NULL)
 		return;
 	for (i = 0; i < n; i++)
 	{
