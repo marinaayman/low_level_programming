a87e764eccb87ab084336762ce1648f60c47c1ac
marinaayman <marinaaymankaram@gmail.com>
2023-04-19 18:27:25 -0700

    task3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,7 +1,7 @@
 #ifndef _3_CALC_H
 #define _3_CALC_H
 #include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct op - Struct op
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(operator) == NULL)
+	if (get_op_func(operator) == NULL || *(operator + 1) != '\0')
 	{
 		printf("Error\n");
 		exit(99);
