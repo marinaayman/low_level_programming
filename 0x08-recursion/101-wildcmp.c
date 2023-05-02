f1a64815fa1fabe96943c6702af6d59b5fc9a76c
marinaayman <marinaaymankaram@gmail.com>
2023-05-02 13:15:25 -0700

    task8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,7 +4,7 @@
   * wildcmp- compares two strings.
   * @s1: string 1.
   * @s2: string 2.
-  * Retrun: 1 if the strings can be considered identical,
+  * Return: 1 if the strings can be considered identical,
   *         otherwise 0.
   */
 
