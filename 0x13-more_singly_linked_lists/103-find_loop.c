b4e4e293d936c18e44f856919468462d061f8fe8
marinaayman <marinaaymankaram@gmail.com>
2023-05-02 06:49:00 -0700

    task14

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+  * find_listint_loop- checks if there is a loop.
+  * @head: a pointer of type struct listint_t.
+  * Return: pointer if loop.
+  *         null if not loop.
+  */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	slow = fast = head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+
+			while (fast != slow)
+			{
+				fast = fast->next;
+				slow = slow->next;
+			}
+			return (fast);
+		}
+	}
+	return (NULL);	
+}
