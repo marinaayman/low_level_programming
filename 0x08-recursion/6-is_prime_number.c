2102d4e752754c76c5fc9f4d495187d92e4bc678
marinaayman <marinaaymankaram@gmail.com>
2023-05-02 11:50:43 -0700

    task6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * isprime- checks primality.
+  * @n: number.
+  * @i: iterator.
+  * Return: 1, it not primality.
+  *         0, otherwise.
+  */
+
+int isprime(int n, int i)
+{
+	int c = 0;
+	
+	if (i <= n)
+	{	
+		if (n % i == 0)
+		{	
+			c++;
+			isprime(n, i + 1);
+		}
+	}
+	return (c);
+}
+/**
+  * is_prime_number- checks if the number is prime.
+  * @n: an integer value to be checked for primality.
+  * Return: 1 if the input integer is a prime number,
+  *         0, otherwise.
+  */
+
+int is_prime_number(int n)
+{
+	int c = 0;
+
+	if (n <= 1)
+		return (0);
+	c = isprime(n, 2);
+	if (c == 1)
+		return (1);
+	else
+		return (0);
+}
