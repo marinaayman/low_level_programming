fa489179096dedc02b17a3fdccd80b080bcb7c40
marinaayman <marinaaymankaram@gmail.com>
2023-05-02 22:09:51 -0700

    task12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -75,7 +75,7 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *ptr = NULL, *loophead = NULL;
 
 	if (head == NULL)
-		return (NULL);
+		return (nodes_num);
 	ptr = detect_loop(head);
 	if (ptr != NULL)
 	{
