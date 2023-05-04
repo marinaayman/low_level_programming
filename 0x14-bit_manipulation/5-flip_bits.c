7b5fb9ff9932a04c735ed3f128a1e7b49baca75b
marinaayman <marinaaymankaram@gmail.com>
2023-05-04 14:38:17 -0700

    task5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,13 +10,5 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m;
-	unsigned int count = 0;
-
-	while (xor)
-	{
-		count += xor & 1;
-		xor >>= 1;
-	}
-	return count;
+	return (__builtin_popcountl(n ^ m));
 }
