06ff19b0f340fc7f59ba32a77e7b3cfa9ab03b11
marinaayman <marinaaymankaram@gmail.com>
2023-05-04 15:11:36 -0700

    task6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * get_endianness- checks the endianness.
   * Return: 0 if big endian, 1 if little endian.
@@ -7,10 +6,30 @@
 
 int get_endianness(void)
 {
-	unsigned int x = 1;
-	char *c = (char *) &x;
+	/**
+	  * x- A union that contains a short unsigned int and a struct.
+	  * @i: A short unsigned int member.
+	  * @c: A struct with two unsigned char members.
+	  */
 
-	if (*c)
+	union
+	{
+		unsigned short int i;
+		/**
+		  * c- Astruct.
+		  * @c0: member one.
+		  * @c1: member two.
+		  */
+		struct
+		{
+			unsigned char c0;
+			unsigned char c1;
+		} c;
+	} x;
+
+	x.i = 1;
+
+	if (x.c.c0)
 		return (1);
 	else
 		return (0);
